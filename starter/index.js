be525e885a6a11d5c0264e19fc6e2e9ad62b9d45
thelukass88 <l.holliday88@hotmail.com>
2024-02-17 08:34:23 +0000

    added questions

--- starter/index.js
+++ starter/index.js
@@ -5,11 +5,75 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "Title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "table"
+
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project: "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation process if any: ",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is this project usage for?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Chose the license you picked for this project: ",
+        choices: [
+            "Apache",
+            "Academic",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Who are the contributors of this projects?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Is there a test included?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "What do I do if I have an issue? "
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your GitHub username: "
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email: "
+    }
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    
 }
 
 // function to initialize program
