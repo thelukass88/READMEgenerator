777aad35860951db2fd62f4be6767224c88da4dc
thelukass88 <l.holliday88@hotmail.com>
2024-02-17 08:50:46 +0000

    added generateMarkdown data

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,6 +1,24 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description
+${data.description}
+## Installation
+${data.installation}
+## Useage
+${data.useage}
+## Licence
+${data.licence}
+## Contributing
+${data.contributing}
+## Tests
+${data.tests}
+## Questions
+${data.questions}
+## Username
+${data.username}
+## Email
+${data.email}
 
 `;
 }
